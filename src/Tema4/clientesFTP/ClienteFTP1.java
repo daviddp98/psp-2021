6415644c520fff7e16487a1fcaef753d0fa23db7
daviddp98 <daviddp98@gmail.com>
2021-02-01 17:26:57 +0100

    v1

---
+++ src/Tema4/clientesFTP/ClienteFTP1.java
@@ -0,0 +1,34 @@
+package Tema4.clientesFTP;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
+
+import java.io.IOException;
+import java.net.SocketException;
+
+public class ClienteFTP1 {
+    public static void main(String[] args) throws SocketException, IOException {
+
+        FTPClient cliente = new FTPClient();
+        String servFTP = "ftp.rediris.es"; // servidor FTP
+        System.out.println("Nos conectamos a: " + servFTP);
+        cliente.connect(servFTP);
+
+        // respuesta del servidor FTP
+        System.out.print(cliente.getReplyString());
+        // código de respuesta
+        int respuesta = cliente.getReplyCode();
+
+        System.out.println("Respuesta: " + respuesta);
+
+        // comprobación del código de respuesta
+        if (!FTPReply.isPositiveCompletion(respuesta)) {
+            cliente.disconnect();
+            System.out.println("Conexión rechazada: " + respuesta);
+            System.exit(0);
+        }
+        // desconexión del servidor FTP
+        cliente.disconnect();
+        System.out.println("Conexión finalizada.");
+    }
+}
